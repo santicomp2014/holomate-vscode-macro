686d48c4bd090d3c173bcf33b07b9314a72f5afa
Santiago Regusci <santicomp2@gmail.com>
2023-02-17 11:10:45 -0300

    first working example

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { window, commands, ExtensionContext } from 'vscode';
-import { WebMidi } from 'webmidi';
+
+// @ts-ignore
+import navigator = require('web-midi-api');
 
 //import { showQuickPick, showInputBox } from './basicInput';
 //import { multiStepInput } from './multiStepInput';
@@ -33,25 +35,51 @@ import { WebMidi } from 'webmidi';
 
 export function activate(context: ExtensionContext) {
 	console.log('MIDI extension activated');
-
-	console.log("Supported",WebMidi.supported);
-
-	WebMidi
-	.enable()
-	.then(() => {
-		console.log("WebMidi enabled!");
-		const holomate = WebMidi.getInputByName("Holomate");
-		if (!holomate) {
-			console.log("Holomate not found");
-			return;
-		}
-		holomate.addListener("noteon", e => {
-			console.log(e.note.identifier);
+	//Request MIDI access
+	navigator.requestMIDIAccess()
+		.then(onMIDISuccess, onMIDIFailure)
+		.catch(e => {
+			console.error(e);
 		});
-	})
-	.catch(err => console.error("WebMidi Error!",err));
 
-	
+	// Function to handle MIDI access success
+	function onMIDISuccess(midiAccess) {
+		console.log("MIDI access available");
+
+		// Get the first MIDI input device
+		var input = midiAccess.inputs.values().next().value;
+
+		// If there is no input device, show an error message
+		if (!input) {
+		console.log("No MIDI input devices found");
+		return;
+		}
+
+		// Set up a listener for MIDI messages
+		input.onmidimessage = function(message) {
+		// Extract the MIDI message data
+		var status = message.data[0] & 0xf0;
+		var channel = message.data[0] & 0x0f;
+		var data1 = message.data[1];
+		var data2 = message.data[2];
+
+		// Handle the MIDI message based on the status byte
+		switch (status) {
+			case 0x90: // Note on
+			// Display a message box with the note number and velocity
+			//vscode.window.showInformationMessage("Note on: channel=" + channel + ", note=" + data1 + ", velocity=" + data2);
+			console.log("Note on: channel=" + channel + ", note=" + data1 + ", velocity=" + data2)
+			break;
+			// Add cases for other MIDI message types as needed
+		}
+		};
+	}
+
+	// Function to handle MIDI access failure
+	function onMIDIFailure(error) {
+		console.log("Failed to get MIDI access - " + error);
+	}
+
 }
 export function deactivate(context: ExtensionContext) {
 	console.log('MIDI extension deactivated');	
